af5b37614109155ead52a8d08f88e4040d50c20c
Yuri Mota <yurirm55@gmail.com>
2023-02-28 15:30:51 -0300

    fix: conserta typescript

--- src/components/ProductList/ProductCard/index.tsx
+++ src/components/ProductList/ProductCard/index.tsx
@@ -2,40 +2,40 @@ import { useContext } from 'react';
 import { StyledProductCard } from './style';
 import { StyledButton } from '../../../styles/button';
 import { StyledParagraph, StyledTitle } from '../../../styles/typography';
-import { CartContext, IProduct } from '../../../contexts/CartContext';
+import { CartContext } from '../../../contexts/CartContext';
 
 const ProductCard = () => {
   const { listProducts, addToCart } = useContext(CartContext);
 
-  const handleAddToCart = (product: IProduct) => {
-    addToCart(product);
-  };
-
-  return listProducts.map((product) => (
-    <StyledProductCard key={crypto.randomUUID()}>
-      <div className='imageBox'>
-        <img src={product.img} alt={product.name} />
-      </div>
-      <div className='content'>
-        <StyledTitle tag='h3' $fontSize='three'>
-          {product.name}
-        </StyledTitle>
-        <StyledParagraph className='category'>
-          {product.category}
-        </StyledParagraph>
-        <StyledParagraph className='price'>
-          R$ {product.price.toFixed(2)}
-        </StyledParagraph>
-        <StyledButton
-          $buttonSize='medium'
-          $buttonStyle='green'
-          onClick={() => handleAddToCart(product)}
-        >
-          Adicionar
-        </StyledButton>
-      </div>
-    </StyledProductCard>
-  ));
+  return (
+    <>
+      {listProducts.map((product) => (
+        <StyledProductCard key={crypto.randomUUID()}>
+          <div className='imageBox'>
+            <img src={product.img} alt={product.name} />
+          </div>
+          <div className='content'>
+            <StyledTitle tag='h3' $fontSize='three'>
+              {product.name}
+            </StyledTitle>
+            <StyledParagraph className='category'>
+              {product.category}
+            </StyledParagraph>
+            <StyledParagraph className='price'>
+              R$ {product.price.toFixed(2)}
+            </StyledParagraph>
+            <StyledButton
+              $buttonSize='medium'
+              $buttonStyle='green'
+              onClick={() => addToCart(product)}
+            >
+              Adicionar
+            </StyledButton>
+          </div>
+        </StyledProductCard>
+      ))}
+    </>
+  );
 };
 
 export default ProductCard;
