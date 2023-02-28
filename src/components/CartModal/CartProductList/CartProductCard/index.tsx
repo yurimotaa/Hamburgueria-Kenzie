ff7b483e5b675166a1428cd2591aa355c8545def
Yuri Mota <yurirm55@gmail.com>
2023-02-28 14:18:53 -0300

    fix: erro typescript

--- src/components/CartModal/CartProductList/CartProductCard/index.tsx
+++ src/components/CartModal/CartProductList/CartProductCard/index.tsx
@@ -4,31 +4,34 @@ import { StyledCartProductCard } from './style';
 import { StyledTitle } from '../../../../styles/typography';
 import { CartContext } from '../../../../contexts/CartContext';
 
-// eslint-disable-next-line consistent-return
 const CartProductCard = () => {
   const { listCart, deleteProduct } = useContext(CartContext);
 
-  if (listCart.length > 0) {
-    return listCart.map((product) => (
-      <StyledCartProductCard key={crypto.randomUUID()}>
-        <div className='imageBox'>
-          <img src={product.img} alt={product.name} />
-        </div>
-        <div className='contentBox'>
-          <StyledTitle tag='h3' $fontSize='three'>
-            {product.name}
-          </StyledTitle>
-          <button
-            type='button'
-            aria-label='Remover'
-            onClick={() => deleteProduct(product)}
-          >
-            <MdDelete size={24} />
-          </button>
-        </div>
-      </StyledCartProductCard>
-    ));
-  }
+  return (
+    <div>
+      {listCart.length > 0
+        ? listCart.map((product) => (
+            <StyledCartProductCard key={crypto.randomUUID()}>
+              <div className='imageBox'>
+                <img src={product.img} alt={product.name} />
+              </div>
+              <div className='contentBox'>
+                <StyledTitle tag='h3' $fontSize='three'>
+                  {product.name}
+                </StyledTitle>
+                <button
+                  type='button'
+                  aria-label='Remover'
+                  onClick={() => deleteProduct(product)}
+                >
+                  <MdDelete size={24} />
+                </button>
+              </div>
+            </StyledCartProductCard>
+          ))
+        : null}
+    </div>
+  );
 };
 
 export default CartProductCard;
